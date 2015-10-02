47e61191596261c4b8db6531d207c4876540d51f
Thiago Lagden <lagden@gmail.com>
2015-10-01 21:42:03 -0300

    added exit

--- bin/deploy-gh-pages.sh
+++ bin/deploy-gh-pages.sh
@@ -10,3 +10,4 @@ git config user.email "travis@lagden.in"
 git add .
 git commit -m "Deploy to GitHub Pages"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+exit 0
